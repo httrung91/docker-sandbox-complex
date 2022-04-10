a81ad992ff08eb83179644cd7b5f482eb3ea4920
trunght <httrung91@gmail.com>
2022-04-10 12:48:01 +0700

    nginx

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -17,8 +17,10 @@ class App extends Component{
             <Link to="/otherpage">OtherPage</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Routes>
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />
+            </Routes>
           </div>
         </div>
       </Router>
